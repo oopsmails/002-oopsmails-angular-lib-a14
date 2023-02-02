68362447834e449c37b3883f610bc21cad45a973
oopsmails <oopsmails@gmail.com>
2023-02-02 16:12:25 -0500

    minor

--- src/app/components/hello.component.ts
+++ src/app/components/hello.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OopsLib001Service } from 'oops-lib001';
-import { OopsLib002Service, RandomItem, SharedDataService } from 'oops-lib002';
+import {
+  OopsLib002Service,
+  RandomItem,
+  SharedDataService,
+  StateService,
+  UsState,
+} from 'oops-lib002';
 import { Observable, Subject } from 'rxjs';
 
 @Component({
@@ -13,18 +19,21 @@ export class HelloComponent implements OnInit, OnDestroy {
   private onDestroy$: Subject<boolean> = new Subject();
   projectDescription: String = 'Testing Angular Lib Sharing';
   randomItems$: Observable<RandomItem[]>;
+  states$: Observable<UsState[]>;
 
   constructor(
     private router: Router,
     private oopsLib001Service: OopsLib001Service,
     private oopsLib002Service: OopsLib002Service,
-    private sharedDataService: SharedDataService
+    private sharedDataService: SharedDataService,
+    private stateService: StateService
   ) {}
 
   ngOnInit() {
     this.oopsLib001Service.doSomething();
     this.oopsLib002Service.doSomething();
     this.randomItems$ = this.sharedDataService.getRandomItems(30, 2000);
+    this.states$ = this.stateService.getUsStates();
   }
 
   navToPage(page) {
